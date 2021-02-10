bb0313571b4aa99df5569e2c8a9538fffde655da
Juan Peñaherrera <juangps96@hotmail.com>
2021-02-10 16:20:48 -0500

    Add files via upload

---
+++ webappcore1/BusinessLogic/MathBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webappcore1.BusinessLogic
+{
+    public class MathBL
+    {
+        public int Sum(int num1, int num2)
+        {
+            int _sum = num1 + num2;
+            return _sum;
+
+        }
+
+    }
+}
